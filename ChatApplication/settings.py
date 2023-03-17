65f1d41ec56ec05c17fb759a02c988d71ddb861d
AswathiMohan23 <achu999here@gmail.com>
2023-03-17 15:01:59 +0530

    [ASWATHI] ADDED . index.html file for ChatApplication

--- ChatApplication/settings.py
+++ ChatApplication/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'chat_app',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_APPLICATION = "ChatApplication.asgi.application"
